c392d5a3c4daccdf3a5bf1da9cc6b6ef7496fd27
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2022-08-05 21:37:02 +0300

    Test begn

--- test/SomeShopERC721-test.js
+++ test/SomeShopERC721-test.js
@@ -2,5 +2,18 @@ const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
 describe("ERC721Tests", function() {
+    let owner
+    let buyer
+    let buyer2
+    let token
 
+    beforeEach(async function() {
+        [owner, buyer, buyer2] = await ethers.getSigners()
+
+        const ERC721 = await ethers.getContractFactory("ERC721", owner)
+        token = await ERC721.deploy("Yarik721", "YRK")
+        await ERC721.deployed()
+
+        shop.setToken(token.address)
+    })
 })
